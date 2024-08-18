007795c1a587e6a2a8e0eb60812b6d584e241351
Joel Spadin <joelspadin@gmail.com>
2024-08-18 12:46:26 -0500

    Silence pylint in backports

--- zmk/backports.py
+++ zmk/backports.py
@@ -3,6 +3,8 @@ Backports from Python > 3.10.
 """
 
 try:
+    # pylint: disable=unused-import
     from enum import StrEnum
 except ImportError:
+    # pylint: disable=unused-import
     from backports.strenum import StrEnum
