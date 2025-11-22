5d43d6dd147f7a4310c0647d40badf1307aaab0a
Joel Spadin <joelspadin@gmail.com>
2025-11-22 00:29:08 -0600

    feat: Support board revisions in keyboard add/list commands
    "zmk keyboard add" nows understand board revisions.
    
    If you attempt to add a board which has multiple revisions (and you
    don't provide a revision with a parameter like "-c nice_nano@2"), it
    will now prompt you to select a revision.
    
    When copying .keymap and .conf files to your config repo, if the
    keyboard is a board with a revision, it will now check for versioned
    files before checking for a common file with no revision.
    
    "zmk keyboard list" now supports a --revisions flag which prints each
    board revision as a separate item.

--- zmk/styles.py
+++ zmk/styles.py
@@ -2,7 +2,9 @@
 Console styling utilities.
 """
 
+from rich.console import HighlighterType
 from rich.highlighter import RegexHighlighter
+from rich.text import Text
 from rich.theme import Theme
 
 
@@ -12,10 +14,46 @@ class KeyValueHighlighter(RegexHighlighter):
     highlights = [r"(?P<key>[\w.]+)(?P<equals>=)(?P<value>.*)"]
 
 
-KEY_VALUE_THEME = Theme(
+class BoardIdHighlighter(RegexHighlighter):
+    """Highlights Zephyr board IDs"""
+
+    base_style = "board."
+
+    highlights = [
+        r"(?P<separator>[/])",
+        r"(?P<revision>@(?:[A-Z]|([0-9]+(\.[0-9]+){0,2})))",
+    ]
+
+
+class CommandLineHighlighter(RegexHighlighter):
+    """Highlights command line arguments"""
+
+    base_style = "cmd."
+
+    highlights = [r"(?P<flag>-[a-zA-Z]|--[a-zA-Z-_]+)"]
+
+
+THEME = Theme(
     {
         "key": "bright_blue",
         "equals": "dim blue",
         "value": "default",
+        "board.separator": "dim",
+        "board.revision": "sky_blue2",
+        "cmd.flag": "dim",
     }
 )
+
+
+def chain_highlighters(highlighters: list[HighlighterType]) -> HighlighterType:
+    """Return a new highlighter which runs each of the given highlighters in order"""
+
+    def run_all_highlighters(text: str | Text):
+        text = text if isinstance(text, Text) else Text(text)
+
+        for item in highlighters:
+            text = item(text)
+
+        return text
+
+    return run_all_highlighters
